bcfd3075e7636db415a8957fa394fd4fd23753f8
danielsm27 <danielsm27@hotmail.com>
2023-10-05 17:57:43 -0600

    3er Commit de la tercera semana

---
+++ src/test/java/com/tarea2_v1/Tarea2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea2_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
